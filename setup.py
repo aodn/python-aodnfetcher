6f69cf16dc8acb81d79c621570cf9cb3851ccd06
Leigh Gordon <leigh.gordon@utas.edu.au>
2018-04-18 12:22:29 +1000

    Initial commit of aodnfetcher

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='aodnfetcher',
+    version='0.1',
+    packages=['aodnfetcher'],
+    url='https://github.com/aodn',
+    license='GPLv3',
+    author='AODN',
+    author_email='developers@emii.org.au',
+    description='AODN artifact fetcher',
+    zip_safe=False,
+    install_requires=['boto3'],
+    tests_require=['mock'],
+    entry_points={'console_scripts': ['aodnfetcher=aodnfetcher.cli:main']}
+)
---
+++ tests/test_aodnfetcher.py
@@ -0,0 +1,211 @@
+import os
+import unittest
+from urlparse import urlparse
+
+import botocore.exceptions
+import mock
+
+import aodnfetcher
+import aodnfetcher.fetcherlib
+
+
+class TestFetcherLib(unittest.TestCase):
+    @mock.patch('aodnfetcher.fetcherlib.os')
+    def test_caching_downloader(self, mock_os):
+        downloader = aodnfetcher.fetcher_downloader('cache_dir')
+        self.assertIsInstance(downloader, aodnfetcher.fetcherlib.FetcherCachingDownloader)
+
+    def test_direct_downloader(self):
+        downloader = aodnfetcher.fetcher_downloader()
+        self.assertIsInstance(downloader, aodnfetcher.fetcherlib.FetcherDirectDownloader)
+
+    @mock.patch('aodnfetcher.fetcherlib.urllib2')
+    def test_http_scheme(self, mock_urllib2):
+        fetcher = aodnfetcher.fetcher('http://www.example.com')
+        self.assertIsInstance(fetcher, aodnfetcher.fetcherlib.HTTPFetcher)
+
+    @mock.patch('aodnfetcher.fetcherlib.urllib2')
+    def test_https_scheme(self, mock_urllib2):
+        fetcher = aodnfetcher.fetcher('https://www.example.com')
+        self.assertIsInstance(fetcher, aodnfetcher.fetcherlib.HTTPFetcher)
+
+    def test_local_scheme(self):
+        expected_relative_path = os.path.join(os.getcwd(), 'path/to/file')
+
+        absolute_fetcher = aodnfetcher.fetcher('/path/to/file')
+        self.assertIsInstance(absolute_fetcher, aodnfetcher.fetcherlib.LocalFileFetcher)
+        self.assertEqual(absolute_fetcher.path, '/path/to/file')
+
+        relative_fetcher = aodnfetcher.fetcher('path/to/file')
+        self.assertIsInstance(relative_fetcher, aodnfetcher.fetcherlib.LocalFileFetcher)
+        self.assertEqual(relative_fetcher.path, expected_relative_path)
+
+        scheme_fetcher = aodnfetcher.fetcher('file://path/to/file')
+        self.assertIsInstance(scheme_fetcher, aodnfetcher.fetcherlib.LocalFileFetcher)
+        self.assertEqual(scheme_fetcher.path, expected_relative_path)
+
+    @mock.patch('aodnfetcher.fetcherlib.boto3')
+    @mock.patch('aodnfetcher.fetcherlib.botocore')
+    def test_jenkins_scheme(self, mock_botocore, mock_boto3):
+        fetcher = aodnfetcher.fetcher('jenkins://bucket/job')
+        self.assertIsInstance(fetcher, aodnfetcher.fetcherlib.JenkinsS3Fetcher)
+        self.assertEqual(fetcher.bucket, 'bucket')
+        self.assertEqual(fetcher.job_name, 'job')
+
+    @mock.patch('aodnfetcher.fetcherlib.boto3')
+    @mock.patch('aodnfetcher.fetcherlib.botocore')
+    def test_s3_scheme(self, mock_botocore, mock_boto3):
+        fetcher = aodnfetcher.fetcher('s3://bucket/key/path')
+        self.assertIsInstance(fetcher, aodnfetcher.fetcherlib.S3Fetcher)
+        self.assertEqual(fetcher.bucket, 'bucket')
+        self.assertEqual(fetcher.path, 'key/path')
+
+    def test_invalid_scheme(self):
+        with self.assertRaises(aodnfetcher.fetcherlib.InvalidArtifactError):
+            _ = aodnfetcher.fetcher('invalidscheme://invalid/scheme')
+
+
+# TODO: write tests for FetcherCachingDownloader
+class TestFetcherCachingDownloader(unittest.TestCase):
+    pass
+
+
+class TestFetcherDirectDownloader(unittest.TestCase):
+    def test_handle(self):
+        fetcher = aodnfetcher.fetcher('path/to/file')
+        downloader = aodnfetcher.fetcher_downloader()
+        with mock.patch('aodnfetcher.fetcherlib.open', mock.mock_open()):
+            self.assertEqual(fetcher.handle, downloader.get_handle(fetcher))
+
+
+class TestHTTPFetcher(unittest.TestCase):
+    def setUp(self):
+        self.url = 'http://www.example.com'
+        self.fetcher = aodnfetcher.fetcherlib.HTTPFetcher(urlparse(self.url))
+        self.mock_content = 'mock content'
+        self.mock_etag = 'abc123'
+
+    @mock.patch('aodnfetcher.fetcherlib.urllib2')
+    def test_handle(self, mock_urllib2):
+        mock_urllib2.build_opener().open().fp.read.return_value = self.mock_content
+        content = self.fetcher.handle.read()
+        self.assertEqual(content, self.mock_content)
+
+    def test_real_url(self):
+        self.assertEqual(self.fetcher.real_url, self.url)
+
+    @mock.patch('aodnfetcher.fetcherlib.urllib2')
+    def test_unique_id(self, mock_urllib2):
+        mock_urllib2.build_opener().open().headers.get.return_value = self.mock_etag
+        unique_id = self.fetcher.unique_id
+        self.assertEqual(unique_id, self.mock_etag)
+
+
+class TestLocalFileFetcher(unittest.TestCase):
+    def setUp(self):
+        self.url = 'file://test/file'
+        self.fetcher = aodnfetcher.fetcherlib.LocalFileFetcher(urlparse(self.url))
+        self.mock_content = 'mock content'
+        self.mock_file = mock.mock_open(read_data=self.mock_content)
+
+    def test_handle(self):
+        with mock.patch('aodnfetcher.fetcherlib.open', self.mock_file):
+            handle = self.fetcher.handle
+        content = handle.read()
+        self.assertEqual(content, self.mock_content)
+
+    def test_real_url(self):
+        self.assertEqual(self.fetcher.real_url, self.fetcher.path)
+
+    def test_unique_id(self):
+        mock_content_checksum = '05db393b05821f1a536ec7e7a4094abc67c6293b6489db31d70defcfa60f6a8a'
+
+        with mock.patch('aodnfetcher.fetcherlib.open', self.mock_file):
+            unique_id = self.fetcher.unique_id
+        self.assertEqual(unique_id, mock_content_checksum)
+
+
+class TestS3Fetcher(unittest.TestCase):
+    def setUp(self):
+        self.url = 's3://bucket/key/path'
+        with mock.patch('aodnfetcher.fetcherlib.boto3'):
+            self.fetcher = aodnfetcher.fetcherlib.S3Fetcher(urlparse(self.url))
+        self.mock_content = 'mock content'
+        self.mock_etag = 'abc123'
+        mock_body = mock.MagicMock()
+        mock_body.read.return_value = self.mock_content
+        self.fetcher.s3_client.get_object.return_value = {
+            'Body': mock_body,
+            'ResponseMetadata': {
+                'HTTPHeaders': {
+                    'etag': self.mock_etag
+                }
+            }
+        }
+
+    def test_handle(self):
+        content = self.fetcher.handle.read()
+        self.assertEqual(content, self.mock_content)
+
+    def test_real_url(self):
+        self.assertEqual(self.fetcher.real_url, self.url)
+
+    def test_unique_id(self):
+        unique_id = self.fetcher.unique_id
+        self.assertEqual(unique_id, self.mock_etag)
+
+    def test_auth_failure(self):
+        self.fetcher.s3_client.get_object.side_effect = botocore.exceptions.ClientError(
+            {'Error': {'Code': 'AuthorizationHeaderMalformed'}}, 'GetObject')
+        with self.assertRaises(aodnfetcher.fetcherlib.AuthenticationError):
+            _ = self.fetcher.object
+
+
+class TestJenkinsS3Fetcher(unittest.TestCase):
+    def setUp(self):
+        self.url = 'jenkins://bucket/job'
+        with mock.patch('aodnfetcher.fetcherlib.boto3'):
+            self.fetcher = aodnfetcher.fetcherlib.JenkinsS3Fetcher(urlparse(self.url))
+        self.mock_content = 'mock content'
+        self.mock_etag = 'abc123'
+        mock_body = mock.MagicMock()
+        mock_body.read.return_value = self.mock_content
+
+        self.fetcher.s3_client.get_object.return_value = {
+            'Body': mock_body,
+            'ResponseMetadata': {
+                'HTTPHeaders': {
+                    'etag': self.mock_etag
+                }
+            }
+        }
+        self.fetcher.s3_client.list_objects_v2.return_value = {
+            'Contents': [{'Key': 'jobs/job/1/path1.war'}, {'Key': 'jobs/job/2/path2.war'}]
+        }
+
+    def test_handle(self):
+        content = self.fetcher.handle.read()
+        self.assertEqual(content, self.mock_content)
+
+    def test_real_url(self):
+        self.assertEqual(self.fetcher.real_url, 's3://bucket/jobs/job/2/path2.war')
+
+    def test_unique_id(self):
+        unique_id = self.fetcher.unique_id
+        self.assertEqual(unique_id, self.mock_etag)
+
+    def test_auth_failure(self):
+        self.fetcher.s3_client.get_object.side_effect = botocore.exceptions.ClientError(
+            {'Error': {'Code': 'AuthorizationHeaderMalformed'}}, 'GetObject')
+        with self.assertRaises(aodnfetcher.fetcherlib.AuthenticationError):
+            _ = self.fetcher.object
+
+    def test_no_builds(self):
+        self.fetcher.s3_client.list_objects_v2.return_value = {'Contents': []}
+        with self.assertRaises(ValueError):
+            _ = self.fetcher.object
+
+    def test_no_matching_builds(self):
+        self.fetcher.s3_client.list_objects_v2.return_value = {'Contents': [{'Key': 'jobs/job/3/path3.txt'}]}
+        with self.assertRaises(ValueError):
+            _ = self.fetcher.object

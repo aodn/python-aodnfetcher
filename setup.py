69b966c05bc4b51999134f4793cb264e9c4a25ee
Leigh Gordon <leigh.gordon@utas.edu.au>
2018-08-01 15:32:33 +1000

    Update .travis.yml for code coverage testing

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     description='AODN artifact fetcher',
     zip_safe=False,
     install_requires=['boto3'],
-    tests_require=['mock'],
+    tests_require=['mock', 'codecov', 'coverage'],
     entry_points={'console_scripts': ['aodnfetcher=aodnfetcher.cli:main']}
 )

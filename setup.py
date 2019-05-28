9a0dab38eb9b18014033e8a6d4081538e1eb982a
Leigh Gordon <leigh.gordon@utas.edu.au>
2019-05-28 12:55:38 +1000

    Refactor setup.py to standard format for package requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup
 
+INSTALL_REQUIRES = [
+        'boto3>=1.9.156',
+        'fasteners==0.14.1',
+        'requests>=2.12.1,<2.20.0'
+    ]
+
+TESTS_REQUIRE = [
+    'mock'
+]
+
 setup(
     name='aodnfetcher',
     version='0.4',
@@ -10,15 +20,8 @@ setup(
     author_email='developers@emii.org.au',
     description='AODN artifact fetcher',
     zip_safe=False,
-    install_requires=[
-        'boto3>=1.4.4',
-        'fasteners==0.14.1',
-        'requests>=2.12.1,<2.20.0'
-    ],
-    tests_require=[
-        'mock',
-        'codecov',
-        'coverage'
-    ],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'testing': TESTS_REQUIRE},
     entry_points={'console_scripts': ['aodnfetcher=aodnfetcher.cli:main']}
 )

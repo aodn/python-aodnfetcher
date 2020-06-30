494ae9e661caa8fd2124e7c22c669047eb87a723
aodn-ci <info@aodn.org.au>
2020-06-30 04:40:36 +0000

    Bump version to 0.6.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='aodnfetcher',
-    version='0.6.5',
+    version='0.6.6',
     packages=['aodnfetcher'],
     url='https://github.com/aodn',
     license='GPLv3',

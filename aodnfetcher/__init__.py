05fc8bd911339b9afb194d0ad7cc4e272b7a4af6
Stefan Hattrell <29941279+digorgonzola@users.noreply.github.com>
2022-09-12 11:14:32 +1000

    use setuptools_scm for package versioning

--- aodnfetcher/__init__.py
+++ aodnfetcher/__init__.py
@@ -1,1 +1,6 @@
 from .fetcherlib import download_file, fetcher, fetcher_downloader
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "Unknown/Not Installed"
